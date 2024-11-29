0ab5577c08da5989189791895856e5c78eef9db0
Bianncajs <biancajesus.silva@outlook.com>
2024-11-29 12:14:42 -0300

    CRUD 4 - Tema/Tema Controller/Tema Repository

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,12 +1,15 @@
 package com.generation.blogpessoal.model;
 
-import java.time. LocalDateTime;
+import java.time.LocalDateTime;
 
 import   org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue; import jakarta.persistence.GenerationType;
 import jakarta.persistence. Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence. Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -26,52 +29,56 @@ public class Postagem {
 	
 	
 	@NotBlank(message = "O atributo texto é Obrigatório!")
-	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres") private String texto;
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres") 
+	private String texto;
 	
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 
 	public Long getId() {
-		return id;
-	}
+        return this.id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public String getTitulo() {
+        return this.titulo;
+    }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	public String getTitulo() {
-		return titulo;
-	}
+    public String getTexto() {
+        return this.texto;
+    }
 
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public LocalDateTime getData() {
+        return this.data;
+    }
 
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+  
+    public Tema getTema() {
+        return this.tema;
+    }
 
-	public String getTexto() {
-		return texto;
-	}
-
-
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
-
-
-	public LocalDateTime getData() {
-		return data;
-	}
-
-
-	public void setData(LocalDateTime data) {
-		this.data = data;
-	}
-	
-	
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
 
 }
